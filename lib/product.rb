1fc3f628c1f7c9e332647499e02f06b0dc547b9b
McFunjy <barminov_01@mail.ru>
2022-11-15 15:25:01 +0300

    Added method update

--- lib/product.rb
+++ lib/product.rb
@@ -9,4 +9,9 @@ class Product
   def to_s
     "#{@price} руб. (осталось #{@count})"
   end
+
+  def update(params)
+    @price = params[:price] if params[:price]
+    @count = params[:count] if params[:count]
+  end
 end
